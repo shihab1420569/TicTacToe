bb86958fd06a60dc23d914161b012cbf358789e2
cse101 <1420569@iub.edu.bd>
2017-06-04 10:59:50 +0600

    assignment 6

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,30 @@
-//JS code goes here
+var click_checker = [0,0,0,0,0,0,0,0,0];
+var game_status = false;
+var turn;
+
+var palyer_status=true;
+
+function game_play(event)
+{
+  var check_player=event.target;
+  if(click_checker[check_player.id]==0)
+  {
+
+
+  event.target.innerHTML=turn?"0" :"x";
+  if(player_status==true)
+  {
+    click_checker[check_player.id]=1;
+    player_status=false;
+  }
+  else if(player_status == false){
+    click_checker[check_player.id]=2;
+    player_status= true;
+  }
+  turn=!turn;
+  console.log(click_checker);
+}
+else if(click_checker[check_player]!==0){
+  aler("this is wrong");
+}
+}
